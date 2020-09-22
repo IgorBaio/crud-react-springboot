6c587cc5fd86d8a0ffd80ceeb5281c6fdd9e4636
Igor <igorbaiosoares@gmail.com>
2020-09-22 13:25:53 -0300

    Creation of the query to create the database

---
+++ query.sql
@@ -0,0 +1,11 @@
+create database produtos;
+
+use produtos;
+
+create table tb_produto (`id` int(20) not null auto_increment
+,`nome` varchar(255) DEFAULT NULL,
+`codigo_barras` varchar(255) DEFAULT NULL, 
+`descricao` varchar(255) DEFAULT NULL
+,`categoria` varchar(255) DEFAULT NULL
+,`quantidade` int(20) DEFAULT NULL
+, PRIMARY KEY (`id`));
