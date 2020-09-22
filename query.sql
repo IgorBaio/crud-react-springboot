b9ffaad590e1241a89c47c909ea9f3ffbfe9006a
Igor <igorbaiosoares@gmail.com>
2020-09-22 13:33:25 -0300

    Query adjustment

--- query.sql
+++ query.sql
@@ -2,10 +2,12 @@ create database produtos;
 
 use produtos;
 
-create table tb_produto (`id` int(20) not null auto_increment,
-`nome` varchar(255) DEFAULT NULL,
-`codigo_barras` varchar(255) DEFAULT NULL, 
-`descricao` varchar(255) DEFAULT NULL,
-`categoria` varchar(255) DEFAULT NULL,
-`quantidade` int(20) DEFAULT NULL,
- PRIMARY KEY (`id`));
+create table tb_produto (
+   `id` int(20) not null auto_increment,
+   `nome` varchar(255) DEFAULT NULL,
+   `codigo_barras` varchar(255) DEFAULT NULL, 
+   `descricao` varchar(255) DEFAULT NULL,
+   `categoria` varchar(255) DEFAULT NULL,
+   `quantidade` int(20) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+);
