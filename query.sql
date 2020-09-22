bd44e48b7216116f11132ac92345b22dc392adef
Igor <igorbaiosoares@gmail.com>
2020-09-22 13:27:53 -0300

    /*

--- query.sql
+++ query.sql
@@ -2,10 +2,10 @@ create database produtos;
 
 use produtos;
 
-create table tb_produto (`id` int(20) not null auto_increment
-,`nome` varchar(255) DEFAULT NULL,
+create table tb_produto (`id` int(20) not null auto_increment,
+`nome` varchar(255) DEFAULT NULL,
 `codigo_barras` varchar(255) DEFAULT NULL, 
-`descricao` varchar(255) DEFAULT NULL
-,`categoria` varchar(255) DEFAULT NULL
-,`quantidade` int(20) DEFAULT NULL
-, PRIMARY KEY (`id`));
+`descricao` varchar(255) DEFAULT NULL,
+`categoria` varchar(255) DEFAULT NULL,
+`quantidade` int(20) DEFAULT NULL,
+ PRIMARY KEY (`id`));
